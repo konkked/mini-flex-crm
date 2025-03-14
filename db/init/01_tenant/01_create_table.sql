75c7aa88ee81bf37ea2da7df1f8438624fdba1b3
Charles Keyser <1chkeyser1@gmail.com>
2025-03-14 06:54:20 -0400

    Add theme to tenant

--- db/init/01_tenant/01_create_table.sql
+++ db/init/01_tenant/01_create_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS "tenant" (
     id SERIAL PRIMARY KEY,
     short_id VARCHAR(255) UNIQUE NOT NULL,
     name VARCHAR(255) UNIQUE NOT NULL,
+    theme VARCHAR(255) NOT NULL,
     attributes JSON NULL
 );
