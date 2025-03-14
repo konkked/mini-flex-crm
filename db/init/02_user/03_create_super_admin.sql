375a97781efb06fd170234b1375cf976a2b72e66
Charles Keyser <1chkeyser1@gmail.com>
2025-03-14 04:39:28 -0400

    changed password for initial user.

--- db/init/02_user/03_create_super_admin.sql
+++ db/init/02_user/03_create_super_admin.sql
@@ -3,7 +3,7 @@ DO $$
     BEGIN
         IF NOT EXISTS (SELECT 1 FROM "app_user" WHERE username = 'super_admin') THEN
             INSERT INTO "app_user" (id, username, name, email, password_hash, salt, role, enabled, tenant_id)
-            VALUES (0, 'super_admin', 'Darth Keyser', '1chkeyser1@gmail.com','4JgINHdacI+yXJGuWj1t300vH4aqkts4w6IYh9/qV3s=', 
+            VALUES (0, 'super_admin', 'Darth Keyser', '1chkeyser1@gmail.com','potYG4o6aBHdTPrAJl4rStU1GcW09nQ6wlUiOVH2BS8=', 
                         'g8eY0NC2wVYWA2y4chGP7Q==','admin', true, 0);
             RAISE NOTICE 'Super admin created';
         END IF;
