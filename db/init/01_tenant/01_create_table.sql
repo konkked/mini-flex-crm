9023bc1cc4bbcd1dabaca0dc820bfb0f4b9b3e58
Charles Keyser <1chkeyser1@gmail.com>
2025-03-14 06:53:28 -0400

    Add theme to tenant

--- db/init/01_tenant/01_create_table.sql
+++ db/init/01_tenant/01_create_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "tenant" (
     id SERIAL PRIMARY KEY,
     short_id VARCHAR(255) UNIQUE NOT NULL,
-    attributes JSON NULL,
-    name VARCHAR(255) UNIQUE NOT NULL
+    name VARCHAR(255) UNIQUE NOT NULL,
+    attributes JSON NULL
 );
--- db/init/01_tenant/02_create_root_tenant.sql
+++ db/init/01_tenant/02_create_root_tenant.sql
@@ -1,3 +1,3 @@
 
 -- Insert tenant record with id 0 if it does not exist
-INSERT INTO "tenant" (id, name, short_id) VALUES (0, 'root', 'root') ON CONFLICT (id) DO NOTHING;
+INSERT INTO "tenant" (id, name, short_id, theme) VALUES (0, 'root', 'root', 'professional') ON CONFLICT (id) DO NOTHING;
